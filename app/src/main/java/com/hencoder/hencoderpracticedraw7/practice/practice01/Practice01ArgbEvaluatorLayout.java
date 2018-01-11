0439788951cc71af9fb7285eea54206c8e9ee0be
wuyue <wuyue@lvmama.com>
2018-01-11 10:08:40 +0800

    修改Practice01ArgbEvaluatorLayout.java

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorLayout.java
@@ -39,6 +39,8 @@ public class Practice01ArgbEvaluatorLayout extends RelativeLayout {
             public void onClick(View v) {
                 ObjectAnimator animator = ObjectAnimator.ofInt(view, "color", 0xffff0000, 0xff00ff00);
                 // 在这里使用 ObjectAnimator.setEvaluator() 来设置 ArgbEvaluator，修复闪烁问题
+                //  如果不使用，会把0xffff0000 看成整体数值变化，0xffff0000变到0xff00ff00时，最后两位的00也会变化，如果看成颜色，
+                // 是将0xff00ff00看为4部分，变化的仅为第二第三部分
                 animator.setEvaluator(new ArgbEvaluator());
                 animator.setInterpolator(new LinearInterpolator());
                 animator.setDuration(2000);
